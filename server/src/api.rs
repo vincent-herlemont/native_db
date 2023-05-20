b1a46a0fc4d87b3d614d726c0dd12e1dd03e5a8b
elliot14A <akshithkatkuri14@gmail.com>
2023-05-20 08:38:51 +0530

    feat: added insert function

--- server/src/api.rs
+++ server/src/api.rs
@@ -1,20 +1,22 @@
 use crate::context::{Result, SDBApiContext};
-use axum::extract::{self, Extension};
+use axum::extract::{self, Extension, FromRequest};
 use axum::response::IntoResponse;
-use axum::routing::get as Get;
+use axum::routing::{get as Get, post};
 use axum::{Json, Router};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use struct_db::SDBItem;
 use tokio::sync::Mutex;
 
 pub fn register_routes<T, H>() -> Router
 where
-    T: SDBItem + Serialize + Send + Sync + 'static,
+    T: SDBItem + Serialize + Deserialize<'static> + Send + Sync + 'static,
     H: SDBApiContext<T>,
 {
     let table_name = T::struct_db_schema().table_name;
-    Router::new().route("/get/:id", Get(get::<T, H>))
+    Router::new()
+        .route(&format!("/{}/:id", table_name), Get(get::<T, H>))
+        .route(&format!("/{}", table_name), post(insert::<T, H>))
 }
 
 pub async fn get<T, H>(
@@ -28,4 +30,17 @@ where
     let ctx = ctx.lock().await;
     let res = ctx.get(&id.to_be_bytes()).await?;
     Ok(Json(res))
+}
+
+pub async fn insert<T, H>(
+    extract::Json(data): extract::Json<T>,
+    Extension(ctx): extract::Extension<Arc<Mutex<H>>>,
+) -> Result<impl IntoResponse>
+where
+    T: SDBItem + Deserialize<'static> + Send + Sync + 'static,
+    H: SDBApiContext<T>,
+{
+    let mut ctx = ctx.lock().await;
+    ctx.insert(data).await?;
+    Ok("insert successful")
 }
