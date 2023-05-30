915af96c6eac74eccb7bc0eb0f90b251b382bd37
Vincent Herlemont <vincent@herlemont.fr>
2023-05-30 23:03:45 +0200

    ci: init release

---
+++ version_update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Bash script to update version for struct_db and struct_db_macro
+
+# Semantic release version obtained from argument
+NEW_VERSION=$1
+
+# Exit if NEW_VERSION is not set
+if [ -z "$NEW_VERSION" ]; then
+  echo "NEW_VERSION argument not set"
+  exit 1
+fi
+
+# Directories containing Cargo.toml files to update
+declare -a directories=("." "struct_db_macro")
+
+for directory in "${directories[@]}"
+do
+  # Check if Cargo.toml exists in the directory
+  if [ -f "$directory/Cargo.toml" ]; then
+    echo "Updating version in $directory/Cargo.toml to $NEW_VERSION"
+    # Use sed to find and replace the version string
+    sed -i -E "s/^version = \"[0-9]+\.[0-9]+\.[0-9]+\"/version = \"$NEW_VERSION\"/g" "$directory/Cargo.toml"
+
+    # Update the dependency version for struct_db_macro in struct_db's Cargo.toml
+    if [ "$directory" == "." ]; then
+      sed -i -E "s/struct_db_macro = \{ version = \"[0-9]+\.[0-9]+\.[0-9]+\", path = \"struct_db_macro\" \}/struct_db_macro = { version = \"$NEW_VERSION\", path = \"struct_db_macro\" }/g" "$directory/Cargo.toml"
+    fi
+  fi
+done
