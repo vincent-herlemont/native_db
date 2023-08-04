6bc6dfd1be264b2965838b0ba5b8b45e14cef228
Vincent Herlemont <vincent@herlemont.fr>
2023-08-04 20:10:58 +0200

    chore: remove eyre

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,20 +1,7 @@
 use shortcut_assert_fs::TmpFs;
-use std::sync::Once;
-
-#[allow(dead_code)]
-static INSTALL_ONCE: Once = Once::new();
 
 #[allow(dead_code)]
 pub fn init() -> TmpFs {
-    let mut _installed = false;
-
-    INSTALL_ONCE.call_once(|| {
-        #[cfg(feature = "eyre_support")]
-        {
-            color_eyre::install().unwrap();
-            _installed = true;
-        }
-    });
 
     TmpFs::new().unwrap()
 }
