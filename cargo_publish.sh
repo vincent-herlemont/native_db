a866355e4cf5758f890b9834d5ab99d4a4d2d1fd
vincent-herlemont <vincent@herlemont.fr>
2024-10-12 10:52:23 +0200

    chore: add usage instructions and error handling to cargo_publish.sh for better script execution guidance

--- cargo_publish.sh
+++ cargo_publish.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# How to test:
+# - Use docker `docker run -it --rm -v $(pwd):/mnt/native_db rust:bullseye bash` 
+# - `cd /mnt/native_db`
+# - `export CARGO_TOKEN=<your_cargo_token>`
+# - `./cargo_publish.sh`
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
@@ -8,7 +14,21 @@ set -x
 ARG_TOKEN="--token=$CARGO_TOKEN"
 
 cd $DIR/native_db_macro
-cargo publish $ARG_TOKEN $@
+
+# Temporarily disable 'set -e' to handle the error manually
+set +e
+OUTPUT=$(cargo publish $ARG_TOKEN "$@" 2>&1)
+EXIT_CODE=$?
+set -e
+
+if [ $EXIT_CODE -ne 0 ]; then
+  if echo "$OUTPUT" | grep -q "crate version .* is already uploaded"; then
+    echo "Warning: $OUTPUT"
+  else
+    echo "Error: $OUTPUT"
+    exit $EXIT_CODE
+  fi
+fi
 
 cd $DIR
 cargo publish $ARG_TOKEN $@
